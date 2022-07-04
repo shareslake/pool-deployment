fd663662a7cfbf4f191e9a6c74544bd4366cfadb
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2022-07-04 11:25:54 +0000

    Fix binaries path in run scripts
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- config/run-block-producing.sh
+++ config/run-block-producing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH="/opt/shareslake/lib:${LD_LIBRARY_PATH:-}" /home/shareslake/bin/shareslake-node run \
+LD_LIBRARY_PATH="/opt/shareslake/lib:${LD_LIBRARY_PATH:-}" /opt/shareslake/bin/shareslake-node run \
   --config /opt/shareslake/configuration/configuration-mainnet.yaml \
   --topology /opt/shareslake/configuration/topology.json \
   --database-path /opt/shareslake/node-db \
